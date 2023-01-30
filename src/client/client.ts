5622affb136a0326a0ae544b6baa965351f08347
tadams95 <tyrelleadams28@gmail.com>
2023-01-30 12:09:47 -0800

    Add GUI panel and Object3D

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
+
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -19,7 +22,7 @@ new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.SphereGeometry();
 const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+  color: 0x00ffff,
   wireframe: true,
 });
 
@@ -37,6 +40,29 @@ function onWindowResize() {
 const stats = Stats();
 
 document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const sphereFolder = gui.addFolder("Sphere");
+const sphereRotationFolder = sphereFolder.addFolder("Rotation");
+sphereRotationFolder.add(sphere.rotation, "x", 0, Math.PI * 2);
+sphereRotationFolder.add(sphere.rotation, "y", 0, Math.PI * 2);
+sphereRotationFolder.add(sphere.rotation, "z", 0, Math.PI * 2);
+sphereFolder.open();
+sphereRotationFolder.open();
+
+const spherePositionFolder = sphereFolder.addFolder("Position");
+spherePositionFolder.add(sphere.position, "x", -10, 10, 0.01);
+spherePositionFolder.add(sphere.position, "y", -10, 10, 0.01);
+spherePositionFolder.add(sphere.position, "z", -10, 10, 0.01);
+sphereFolder.open();
+spherePositionFolder.open();
+
+const sphereScaleFolder = sphereFolder.addFolder("Scale");
+sphereScaleFolder.add(sphere.scale, "x", -5, 5);
+sphereScaleFolder.add(sphere.scale, "y", -5, 5);
+sphereScaleFolder.add(sphere.scale, "z", -5, 5);
+sphereFolder.add(sphere, "visible");
+sphereScaleFolder.open();
 
 function animate() {
   requestAnimationFrame(animate);
